9398944bc38b76788cf69d6bd0f10b9a9a3fded3
Niser01 <64512516+Niser01@users.noreply.github.com>
2026-03-15 14:52:26 -0500

    Se inicializa el proyecto

---
+++ src/main/java/com/bolivar/api_gestion_de_polizas/ApiGestionDePolizasApplication.java
@@ -0,0 +1,13 @@
+package com.bolivar.api_gestion_de_polizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionDePolizasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionDePolizasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolivar/api_gestion_de_polizas/ApiGestionDePolizasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolivar.api_gestion_de_polizas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionDePolizasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
